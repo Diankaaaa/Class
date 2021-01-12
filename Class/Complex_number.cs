bf9912449aae25690d27d09beb53dabe740e2fce
diana <diana@DESKTOP-AE9RS0A>
2021-01-12 14:10:34 +0300

    Добавьте файлы проекта.

---
+++ Class/Complex_number.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Complex_number
+    {
+        private double a1; //Действительная часть
+        private double b1; //Мнимая часть
+
+        public Complex_number(double real, double imaginary)
+        {
+            this.a1 = real;
+            this.b1 = imaginary;
+        }
+
+        //Геттеры
+        public double getA1()
+        {
+            return a1;
+        }
+
+        public double getB1()
+        {
+            return b1;
+        }
+
+        //Функция для вычисления модуля комплексного числа
+        public double ABC()
+        {
+            return Math.Sqrt(a1 * a1 + b1 * b1);
+        }
+
+        //Функция вычисления числа, которое является обратным заданному
+        public double getReverseNumberR()
+        {
+            double d = a1 * a1 + b1 * b1;
+            return a1 / d;
+        }
+        public double getReverseNumberB()
+        {
+            double d = a1 * a1 + b1 * b1;
+            return -b1 / d;
+        }
+
+        //Функция вывода
+        public void Print()
+        {
+            Console.WriteLine("\n" +"Модуль числа: " + ABC() + "\n" + "Обратное число: " + getReverseNumberR() + " " + getReverseNumberB() + "\n");
+        }
+    }
+
+}
---
+++ Class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите действительную часть числа: ");
+            double real = double.Parse(Console.ReadLine());
+            Console.Write("Введите мнимую часть числа: ");
+            double im = double.Parse(Console.ReadLine());
+            Complex_number a = new Complex_number(real, im);
+            a.Print();
+            
+        }
+    }
+}
